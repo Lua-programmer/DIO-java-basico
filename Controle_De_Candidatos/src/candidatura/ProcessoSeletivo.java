f6e536b2a424dedae1ef2d43ea07d0945e30e5c7
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-11-09 23:11:46 -0300

    Fazendo ligações para cada candidato

--- Controle_De_Candidatos/src/candidatura/ProcessoSeletivo.java
+++ Controle_De_Candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,6 +1,7 @@
 package candidatura;
 
 import java.util.Locale;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
@@ -12,21 +13,52 @@ public class ProcessoSeletivo {
 //        analizarCandidato(2000.00);
 //        selecaoDeCandidatos();
 
-        imprimirSelecionados();
+//        imprimirSelecionados();
+
+        String[] candidatos = {"Luana", "Zoe", "David", "Elliot", "Joaquim"};
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
     }
 
-    static void imprimirSelecionados(){
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+            }
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + "° tentativa");
+        } else {
+            System.out.println("Não conseguimos contato com " + candidato + ", número máximo tentativas " + tentativasRealizadas + " realizadas");
+        }
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+
+    static void imprimirSelecionados() {
         String[] candidatos = {"Luana", "Zoe", "David", "Elliot", "Joaquim", "Anna", "Marcos", "Marcela", "Peter", "Jacob"};
 
         System.out.println("\nFORI");
         System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
-        for (int i = 0; i < candidatos.length ; i++) {
+        for (int i = 0; i < candidatos.length; i++) {
             System.out.printf("O candidato de n° %d é o candidato %s \n", i, candidatos[i]);
         }
 
         System.out.println("\nFOREACH");
-        for (String candidato: candidatos) {
-            System.out.printf("O candidato selecionado foi  %s \n",candidato);
+        for (String candidato : candidatos) {
+            System.out.printf("O candidato selecionado foi  %s \n", candidato);
         }
 
     }
